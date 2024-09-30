3b64a7b8f8aa543fd300a0c7b9150cc0778324cb
Josh Stringer <joshstringer@live.com>
2024-09-30 11:26:50 -0500

    created side bar, a few buttons, some hover effects, some pixel art, you know, the usual stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,37 @@
 import { useState } from 'react'
 import './App.css'
+import pixelSquare from './assets/PixelSquare.png';
+import pixelBook from './assets/PixelBook.png';
 
 function App() {
 
   return (
     <>
+      <div className='appBox'>
+        <div className='appBoxHeader'>
+          <img src={pixelSquare} alt='Pixel Square' />
+          <h1 className='tiny5-regular'>MOVING-A-SQUARE</h1>
+          <img src={pixelSquare} alt='Pixel Square' />
+        </div>
+        <div className='appBoxBody'>
+          <div className='sideBar'>
+            <button className='inactiveButtonInfo tiny5-regular'>
+              INFO
+              <img src={pixelBook} alt='Pixel Book' />
+            </button>
+            <button className='inactiveButtonSquare tiny5-regular'>
+              SQUARE
+              <img src={pixelSquare} alt='Pixel Square' />
+            </button>
+          </div>
+          <div className='mainContent'>
 
+          </div>
+        </div>
+
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
