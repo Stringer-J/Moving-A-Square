57f6c182a19b857e1c50bdb2280a1637f521e53c
Josh Stringer <joshstringer@live.com>
2024-10-01 15:30:47 -0500

    added controls button to jump page

--- client/src/components/JumpCom/JumpCom.jsx
+++ client/src/components/JumpCom/JumpCom.jsx
@@ -4,6 +4,7 @@ function JumpCom() {
     return (
         <>
             <div className='jumpBox'>
+            <button className='controlButton tiny5-regular'>CONTROLS</button>
                 <div className='insideJumpBox'>
                     
                 </div>
